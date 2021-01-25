fe37e70b7a00d96360e6e628b0323f8c0edf7a7c
Priya Agrawal <priya@Priyas-MacBook-Pro.local>
2021-01-25 14:10:54 +0530

    updatedForm

--- src/component/UserForm.js
+++ src/component/UserForm.js
@@ -18,6 +18,7 @@ const UserForm = (props) => {
     const [certification, setCertification] = useState("")
     const [internship, setInternship] = useState("")
     const [job_pos, setJob_pos] = useState("")
+    
 
     let details = {   
       name : name,
@@ -39,30 +40,31 @@ const UserForm = (props) => {
 
     const handleSubmit = (e) =>{
         e.preventDefault();
-        console.log(name)
+        
         props.history.push("/UserDetail",  { details });
     }
     return (
-    <div className={Container}>
+    <div>
+      <Container>
         <h1 className="mx-5 my-3">User Data</h1>
     <Form className="mx-5 my-3" onSubmit={ handleSubmit }>
       <Row form>
       <Col md={4}>
           <FormGroup>
             <Label for="name">Name</Label>
-            <Input type="text" name="name" id="name" value ={name} onChange={e => setName(e.target.value)} placeholder="Enter Name" />
+            <Input type="text" name="name" id="name" value ={name} onChange={e => setName(e.target.value)} placeholder="Enter Name" required/>
           </FormGroup>
         </Col>
         <Col md={4}>
           <FormGroup>
             <Label for="email">Email</Label>
-            <Input type="email" name="email" id="email" value ={email} onChange={e => setEmail(e.target.value)} placeholder="@" />
+            <Input type="email" name="email" id="email" value ={email} onChange={e => setEmail(e.target.value)} placeholder="@" required/>
           </FormGroup>
         </Col>
         <Col md={4}>
           <FormGroup>
             <Label for="contact">Contact Number</Label>
-            <Input type="tel" name="contact" pattern="[0-9]{10}" id="contact" value ={contact} onChange={e => setContact(e.target.value)} placeholder="Enter Contact Number" />
+            <Input type="tel" name="contact" pattern="[0-9]{10}" id="contact" value ={contact} onChange={e => setContact(e.target.value)} placeholder="Enter Contact Number" required/>
           </FormGroup>
         </Col>
      
@@ -72,13 +74,13 @@ const UserForm = (props) => {
       <Col md={6}>
       <FormGroup>
         <Label for="Address">Address</Label>
-        <Input type="text" name="address" id="Address" value ={add1} onChange={e => setAdd1(e.target.value)} placeholder="1234 Main St"/>
+        <Input type="text" name="address" id="Address" value ={add1} onChange={e => setAdd1(e.target.value)} placeholder="1234 Main St" required/>
       </FormGroup>
       </Col>
       <Col md={6}>
       <FormGroup>
         <Label for="Address2">Address 2</Label>
-        <Input type="text" name="address2" id="Address2" value ={add2} onChange={e => setAdd2(e.target.value)} placeholder="Apartment, studio, or floor"/>
+        <Input type="text" name="address2" id="Address2" value ={add2} onChange={e => setAdd2(e.target.value)} placeholder="Apartment, studio, or floor" required/>
       </FormGroup>
       </Col>
       </Row>
@@ -87,13 +89,13 @@ const UserForm = (props) => {
         <Col md={4}>
           <FormGroup>
             <Label for="exampleCity">City</Label>
-            <Input type="text" name="city" id="exampleCity" value ={city} onChange={e => setCity(e.target.value)} placeholder="City"/>
+            <Input type="text" name="city" id="exampleCity" value ={city} onChange={e => setCity(e.target.value)} placeholder="City" required/>
           </FormGroup>
         </Col>
         <Col md={6}>
           <FormGroup>
             <Label for="state">State</Label>
-            <select name="state" id="state"  value ={statevalue} onChange={e => setStateValue(e.target.value)} className="form-control">
+            <select name="state" id="state"  value ={statevalue} onChange={e => setStateValue(e.target.value)} className="form-control" required>
                 <option value="Andhra Pradesh">Andhra Pradesh</option>
                 <option value="Andaman and Nicobar Islands">Andaman and Nicobar Islands</option>
                 <option value="Arunachal Pradesh">Arunachal Pradesh</option>
@@ -137,7 +139,7 @@ const UserForm = (props) => {
         <Col md={2}>
           <FormGroup>
             <Label for="exampleZip">Zip</Label>
-            <Input type="number" name="zip" id="exampleZip"  value ={zip} onChange={e => setZip(e.target.value)} placeholder="452001"/>
+            <Input type="number" name="zip" id="exampleZip"  value ={zip} onChange={e => setZip(e.target.value)} placeholder="452001" required/>
           </FormGroup>  
         </Col>
       </Row>
@@ -146,19 +148,19 @@ const UserForm = (props) => {
         <Col md={4}>
           <FormGroup>
             <Label for="education">Education</Label>
-            <Input type="text" name="education" id="education" value ={education} onChange={e => setEducation(e.target.value)} placeholder="Degree"/>
+            <Input type="text" name="education" id="education" value ={education} onChange={e => setEducation(e.target.value)} placeholder="Degree" required/>
           </FormGroup>
         </Col>
         <Col md={4}>
           <FormGroup>
             <Label for="skills">Skills</Label>
-            <Input type="text" name="skills" id="skills" value ={skills} onChange={e => setSkills(e.target.value)} placeholder="Skills"/>
+            <Input type="text" name="skills" id="skills" value ={skills} onChange={e => setSkills(e.target.value)} placeholder="Skills" required/>
           </FormGroup>
         </Col>
         <Col md={4}>
           <FormGroup>
             <Label for="exp">Experience</Label>
-            <Input type="number" name="exp" id="exp" value ={exp} onChange={e => setExp(e.target.value)} placeholder="Years"/>
+            <Input type="number" name="exp" id="exp" value ={exp} onChange={e => setExp(e.target.value)} placeholder="Years" required/>
           </FormGroup>  
         </Col>
       </Row>
@@ -167,19 +169,19 @@ const UserForm = (props) => {
         <Col md={4}>
           <FormGroup>
             <Label for="Certification">Certification</Label>
-            <Input type="text" name="certification" id="Certification" value ={certification} onChange={e => setCertification(e.target.value)} placeholder="Certificates"/>
+            <Input type="text" name="certification" id="Certification" value ={certification} onChange={e => setCertification(e.target.value)} placeholder="Certificates" required/>
           </FormGroup>
         </Col>
         <Col md={4}>
           <FormGroup>
             <Label for="Internship">Internship</Label>
-            <Input type="text" name="internship" id="Internship" value ={internship} onChange={e => setInternship(e.target.value)} placeholder="Internship" />
+            <Input type="text" name="internship" id="Internship" value ={internship} onChange={e => setInternship(e.target.value)} placeholder="Internship"  required/>
           </FormGroup>
         </Col>
         <Col md={4}>
           <FormGroup>
             <Label for="Job Position">Job Position</Label>
-            <Input type="text" name="Job Position" id="Job Position" value ={job_pos} onChange={e => setJob_pos(e.target.value)} placeholder="Job Position"/>
+            <Input type="text" name="Job Position" id="Job Position" value ={job_pos} onChange={e => setJob_pos(e.target.value)} placeholder="Job Position"  required/>
           </FormGroup>  
         </Col>
       </Row>
@@ -209,7 +211,7 @@ const UserForm = (props) => {
       </Row>
 
       <FormGroup check>
-        <Input type="checkbox" name="check" id="exampleCheck"/>
+        <Input type="checkbox" name="check" id="exampleCheck" required/>
         <Label for="exampleCheck" check>Check me out</Label>
       </FormGroup>
       <br />
@@ -217,6 +219,7 @@ const UserForm = (props) => {
       <Button type="submit" color="primary">Submit</Button>
       {/* </Link> */}
     </Form>
+    </Container>
     </div>
     );
 }
